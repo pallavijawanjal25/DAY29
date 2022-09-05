ac8ef60857f2a030ccf1f04c366984cccfa5601a
pallavijawanjal <pallavij@gmail.com>
2022-09-05 23:18:11 +0530

    Add

---
+++ LoadCensusData.cs
@@ -0,0 +1,45 @@
+﻿using CensusAnalyzer_Day29;
+
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CensusAnalyzer_Day29.POCO;
+using System.Globalization;
+using CensusAnalyzer_Day29.CustomException;
+
+
+namespace Day29_IndianCensusAnalyzer
+{
+    public class LoadCensusData
+    {
+        List<IndianStateCensusData> indianStateCensusDatas;
+        public int LoadCensusDataIntoList(string filepath)
+        {
+            try
+            {
+                using (StreamReader reader = File.OpenText(filepath))
+
+                using (CsvReader csvreader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    indianStateCensusDatas = csvreader.GetRecords<IndianStateCensusData>().ToList();
+                }
+                return indianStateCensusDatas.Count;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new CecsusAnalyzerException("WrongHeader", "WrongFileType");
+            }
+
+            return indianStateCensusDatas.Count;
+        }
+
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day29_IndianCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to CensusAnayzer Problems");
+        }
+    }
+}
+
